b2c427a59b995e30d948d3925da8ef01941daf70
SamiSuhail <sami.suhail.dev@gmail.com>
2024-09-16 14:25:45 +0300

    fix table name

--- src/MyApp.DbDeploy/Scripts/0002_create_user_management_schema.sql
+++ src/MyApp.DbDeploy/Scripts/0002_create_user_management_schema.sql
@@ -1,13 +1,13 @@
 ﻿CREATE SCHEMA user_management;
 
-CREATE TABLE user_management.change_email_confirmations(
+CREATE TABLE user_management.email_change_confirmations(
 	id			INT			NOT NULL GENERATED ALWAYS AS IDENTITY,
 	user_id		INT			NOT NULL,
 	code		CHAR(6)		NOT NULL,
 	created_at	TIMESTAMP	NOT NULL,
 	
-	CONSTRAINT pk_change_email_confirmations_id			PRIMARY KEY (id),
-	CONSTRAINT fk_change_email_confirmations_user_id	FOREIGN KEY (user_id) REFERENCES auth.users (id),
-	CONSTRAINT uq_change_email_confirmations_user_id	UNIQUE (user_id),
-	CONSTRAINT uq_change_email_confirmations_code		UNIQUE (code)
+	CONSTRAINT pk_email_change_confirmations_id			PRIMARY KEY (id),
+	CONSTRAINT fk_email_change_confirmations_user_id	FOREIGN KEY (user_id) REFERENCES auth.users (id),
+	CONSTRAINT uq_email_change_confirmations_user_id	UNIQUE (user_id),
+	CONSTRAINT uq_email_change_confirmations_code		UNIQUE (code)
 )
