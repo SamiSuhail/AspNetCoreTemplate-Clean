b8781a2b170571fc8625c7f3c85726c055275f69
SamiSuhail <sami.suhail.dev@gmail.com>
2024-09-16 14:26:13 +0300

    add email change confirmation entity

---
+++ src/MyApp.Server/Domain/UserManagement/EmailChangeConfirmation/EmailChangeConfirmationEntity.cs
@@ -0,0 +1,16 @@
+﻿using MyApp.Server.Domain.Shared;
+
+namespace MyApp.Server.Domain.Auth.EmailChangeConfirmation;
+
+public class EmailChangeConfirmationEntity : BaseConfirmationEntity
+{
+    public static EmailChangeConfirmationEntity Create(int userId)
+    {
+        return new()
+        {
+            UserId = userId,
+            Code = GenerateCode(),
+            CreatedAt = DateTime.UtcNow,
+        };
+    }
+}
