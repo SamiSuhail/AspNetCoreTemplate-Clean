1b62c3f109ec49604b0f9694dc8b025e5a9f9d32
SamiSuhail <sami.suhail.dev@gmail.com>
2024-09-16 13:40:13 +0300

    add database script

---
+++ src/MyApp.DbDeploy/Scripts/0002_create_user_management_schema.sql
@@ -0,0 +1,13 @@
+﻿CREATE SCHEMA user_management;
+
+CREATE TABLE user_management.change_email_confirmations(
+	id			INT			NOT NULL GENERATED ALWAYS AS IDENTITY,
+	user_id		INT			NOT NULL,
+	code		CHAR(6)		NOT NULL,
+	created_at	TIMESTAMP	NOT NULL,
+	
+	CONSTRAINT pk_change_email_confirmations_id			PRIMARY KEY (id),
+	CONSTRAINT fk_change_email_confirmations_user_id	FOREIGN KEY (user_id) REFERENCES auth.users (id),
+	CONSTRAINT uq_change_email_confirmations_user_id	UNIQUE (user_id),
+	CONSTRAINT uq_change_email_confirmations_code		UNIQUE (code)
+)
