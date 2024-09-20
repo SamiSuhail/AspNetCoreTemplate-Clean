79083b22575653cd6d91ecf72d117b0041299170
SamiSuhail <sami.suhail.dev@gmail.com>
2024-09-20 09:43:20 +0300

    add validator and transformer for confirm password change

---
+++ src/MyApp.Server/Application/Commands/UserManagement/PasswordUpdate/ConfirmPasswordChange/ConfirmPasswordChangeTransformer.cs
@@ -0,0 +1,13 @@
+﻿using MyApp.Server.Application.Utilities;
+using MyApp.Server.Shared;
+
+namespace MyApp.Server.Application.Commands.UserManagement.PasswordUpdate.ConfirmPasswordChange;
+
+public class ConfirmPasswordChangeTransformer : IRequestTransformer<ConfirmPasswordChangeRequest>
+{
+    public ConfirmPasswordChangeRequest Transform(ConfirmPasswordChangeRequest request)
+        => request with
+        {
+            Code = request.Code.RemoveWhitespace(),
+        };
+}
