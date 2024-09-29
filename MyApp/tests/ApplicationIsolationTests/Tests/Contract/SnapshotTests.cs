b60046afa03fbca36029837ff5510b76049ec3d9
SamiSuhail <sami.suhail.dev@gmail.com>
2024-09-29 17:59:01 +0300

    add snapshot tests for contracts

---
+++ MyApp/tests/ApplicationIsolationTests/Tests/Contract/SnapshotTests.cs
@@ -0,0 +1,24 @@
+﻿namespace MyApp.ApplicationIsolationTests.Tests.Contract;
+
+public class SnapshotTests(AppFactory appFactory) : BaseTest(appFactory)
+{
+    [Fact]
+    public async Task OpenApiSpec_MatchesSnapshot()
+    {
+        var client = AppFactory.CreateClient();
+        var response = await client.GetAsync("/swagger/v1/swagger.json");
+        string openApiSpec = await response.Content.ReadAsStringAsync();
+
+        await Verify(openApiSpec);
+    }
+
+    [Fact]
+    public async Task GraphQLSchema_MatchesSnapshot()
+    {
+        var client = AppFactory.CreateClient();
+        var response = await client.GetAsync("/graphql?sdl");
+        string openApiSpec = await response.Content.ReadAsStringAsync();
+
+        await Verify(openApiSpec);
+    }
+}
