cede26821f4a2beb8df8f9225b550a1e6c14b33f
SamiSuhail <sami.suhail.dev@gmail.com>
2024-10-11 08:25:15 +0300

    add notes

--- MyApp/tests/Tests.System/Tests/SampleTest.cs
+++ MyApp/tests/Tests.System/Tests/SampleTest.cs
@@ -10,7 +10,19 @@ public class SampleTest(TestFixture fixture) : BaseTest(fixture)
         await Task.CompletedTask;
         // To make system tests work we need to be able to create accounts on the fly for the same email
         // For that we need:
-        // - multiple users with same email (organisation/tenant entities?)
+        // - multiple users with same email
+        //  -> instance entity
+        //      -> needs management so system tests can create instances on the fly
+        //          -> needs instance:create scope
+
+        // Tasks in order:
+        //  1) create instances entity in DB
+        //      a) with default seeded "system" instance
+        //      b) cleanup flag on create (filtered? index)
+
+        //  2) create scopes entity in DB
+        //      a) with default "instance:create" scope seeded
+
         // - mailhog uses relay
         // - test email account
 
